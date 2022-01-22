d2c849d0fe107767182b0293df971fcbd1ad87fc
anujpatidar3 <anujpatidar3@gmail.com>
2022-01-22 21:19:39 +0530

    Static Login and SignUp Pages Added

--- pages/signup.js
+++ pages/signup.js
@@ -5,7 +5,8 @@ import LoginBoy from '../public/assets/Icons/LoginBoy.svg'
 import Logo from '../public/assets/Icons/Logo.svg'
 import Link from 'next/link'
 
-export default function Login() {
+
+export default function Signup() {
     return (
         <div className="container md:w-full flex flex-col justify-center items-center font-secondary font-medium mt-20">
             <Image className='mt-20' src={Logo} height={66.5} width={198} alt='Logo' ></Image>
@@ -13,16 +14,18 @@ export default function Login() {
                 <div className=" w-10/12 md:w-1/2 mb-8 verticalLine ">
                     <div className='loginCard  md:mx-10 xl:mx-20'>
                         <form className='flex flex-col'>
-                            <h1 className='mx-5 mt-5 sm:mx-10 sm:mt-10'>Email</h1>
+                            <h1 className='mx-5 mt-5 sm:mx-10 sm:mt-10'>Name</h1>
+                            <input type="text" defaultValue="" className='px-2 mx-5 sm:mx-10 loginForm'></input>
+                            <h1 className='mx-5 mt-3 sm:mx-10 sm:mt-5'>Email</h1>
                             <input type="text" defaultValue="" className='px-2 mx-5 sm:mx-10 loginForm'></input>
                             <h1 className='mx-5 mt-3 sm:mx-10 sm:mt-5'>Password</h1>
                             <input type="password" defaultValue="" className='px-2 mx-5 mb-3 sm:mb-5 sm:mx-10 loginForm'></input>
                             <div className="loginButton ml-5 mb-5 sm:ml-10 sm:mb-10">
-                                <Button style=" py-1 pl-4 font-medium rounded-md text-black ">Log In</Button>
+                                <Button style=" py-1 pl-2.5 font-medium rounded-md text-black ">Sign Up</Button>
                             </div>
                             <div className='flex flex-col sm:flex-row'>
-                                <h4 className="ml-5 sm:ml-10 sm:mb-5">{"Don't Have an Account?"}</h4>
-                                <h7 className="ml-5 mb-5 sm:mb-0 sm:ml-1 signupText"><Link href='/signup'>Sign Up</Link></h7>
+                                <h4 className="ml-5 sm:ml-10 sm:mb-5">{"Already Have an Account?"}</h4>
+                                <h7 className="ml-5 mb-5 sm:mb-0 sm:ml-1 signupText"><Link href='/login'>Sign In</Link></h7>
                             </div>
                         </form>
                     </div>
